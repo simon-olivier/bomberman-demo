2be6df96ca6357ee2bc2b96422a185efa0edcfb4
Simon Olivier <so.simon.olivier@gmail.com>
2018-01-20 18:37:55 +0100

    Add Character

---
+++ Source/BombermanDemo/BombermanCharacter.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BombermanCharacter.h"
+
+#include "GameFramework/CharacterMovementComponent.h"
+
+
+// Sets default values
+ABombermanCharacter::ABombermanCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+}
---
+++ Source/BombermanDemo/BombermanCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BombermanCharacter.generated.h"
+
+UCLASS()
+class BOMBERMANDEMO_API ABombermanCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABombermanCharacter();
+};
